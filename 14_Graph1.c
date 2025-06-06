b92ef229819f0720f7792f8e3f1e58db494aa51e
Sunwoongiii <its.sunwoongiii@gmail.com>
2025-06-06 23:45:41 +0900

    new file:   14_Graph1.c 	new file:   Assignment/avl_tree.exe

---
+++ 14_Graph1.c
@@ -0,0 +1,18 @@
+/*
+Starting at any vertex, going through each edge exactly once and terminating at the start vertex iff the degree of each vertex is even. 
+
+A graph G consists of two sets, V and E.
+V: vertices
+E: set of pairs of vertices, these pairs are called edges. 
+V(G) and E(G) represent the sets of vertices and edges of G
+G = (V, E)
+
+Graph with self loops: A graph that has an edge from a vertex, v, back to itself
+MultiGraph: A graph allowing multiple edges between a pair of nodes
+Complete Graph: A graph with n vertices has exactly n(n-1)/2 edges
+
+(u,v): vertices u and v are adjacent. Edge(u,v) is incident on vertices u and v.
+<u,v>: u is adjacent to v, and v is adjacent from u.
+
+
+*/
